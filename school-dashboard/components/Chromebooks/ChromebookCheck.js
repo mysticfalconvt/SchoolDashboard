570ab098b4d47c3caf873b1742fd128d3bffaa18
MysticfalconVT <49008703+mysticfalconvt@users.noreply.github.com>
2023-08-28 19:16:20 -0400

    improve chromebook check coloring

--- school-dashboard/components/Chromebooks/ChromebookCheck.js
+++ school-dashboard/components/Chromebooks/ChromebookCheck.js
@@ -157,11 +157,16 @@ function SingleChromebookCheckForm({ assignment, refetch }) {
           {assignment?.number}{" "}
           {assignment.student ? `- ${assignment.student.name}` : null}
         </h2>
-        <label htmlFor="status" key={`status-chromebook-${assignment.id}`}>
+        <label
+          htmlFor="status"
+          className="flex"
+          key={`status-chromebook-${assignment.id}`}
+        >
           Status:{" "}
           <select
             name="status"
             id="status"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             onChange={(e) => {
               setMessage(e.target.value);
               if (e.target.value === "As Issued") {
@@ -180,6 +185,7 @@ function SingleChromebookCheckForm({ assignment, refetch }) {
         <label htmlFor="message" key={`message-chromebook-${assignment.id}`}>
           Message
           <input
+            className="bg-gray-50 border border-gray-500 disabled:bg-gray-50 disabled:border-none "
             type="text"
             name="message"
             id="message"
