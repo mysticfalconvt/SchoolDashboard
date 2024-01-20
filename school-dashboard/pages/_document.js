008035073ed221e0439014b5e9bafc0e004e6bba
MysticfalconVT <49008703+mysticfalconvt@users.noreply.github.com>
2024-01-20 06:56:03 -0500

    tracking script

--- school-dashboard/pages/_document.js
+++ school-dashboard/pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, NextScript, Main } from 'next/document';
+import Document, { Html, Head, NextScript, Main } from "next/document";
 // import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -15,7 +15,13 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en-CA">
-      <Head />
+        <Head>
+          <script
+            async
+            src="https://umami.rboskind.com/script.js"
+            data-website-id="8b53e6ab-eeb4-4954-aa7d-d3fb46b22914"
+          ></script>
+        </Head>
         <body>
           <Main />
           <NextScript />
