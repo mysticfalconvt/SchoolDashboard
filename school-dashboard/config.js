050dcb9b1055acc975c2c3b990955bc33b4b833d
MysticfalconVT <49008703+mysticfalconvt@users.noreply.github.com>
2024-07-25 07:33:16 -0400

    endpoint added to env

--- school-dashboard/config.js
+++ school-dashboard/config.js
@@ -1,4 +1,6 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `https://api.ncujhs.tech/api/graphql`;
+export const endpoint =
+  process.env.ENDPOINT || `http://localhost:3000/api/graphql`;
+export const prodEndpoint =
+  process.env.ENDPOINT || `https://api.ncujhs.tech/api/graphql`;
 export const perPage = 4;
