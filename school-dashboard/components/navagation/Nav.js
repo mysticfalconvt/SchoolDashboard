1fb6a6341bac720af4f78768c2532d3407370fe8
MysticfalconVT <49008703+mysticfalconvt@users.noreply.github.com>
2024-08-06 19:14:51 -0400

    hide birthdays

--- school-dashboard/components/navagation/Nav.js
+++ school-dashboard/components/navagation/Nav.js
@@ -53,7 +53,7 @@ export default function Nav() {
           {isAllowed(me, "isSuperAdmin") && (
             <Link href="/superUserSettings">⚙️</Link>
           )}
-          {isAllowed(me, "canManagePbis") && <Link href="/birthdays">🧁</Link>}
+          {/* {isAllowed(me, "canManagePbis") && <Link href="/birthdays">🧁</Link>} */}
         </>
       )}
     </NavStyles>
